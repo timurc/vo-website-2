4da0579e0956776864201c1ec7a29afb567e81b3
Timur Çelikel <me@timurcelikel.com>
2018-06-27 10:31:20 +0200

    do not link to original images

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
                         resolve: `gatsby-remark-images`,
                         options: {
                             maxWidth: 590,
+                            linkImagesToOriginal: false,
                         },
                     },
                     {
