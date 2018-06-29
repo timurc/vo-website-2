a043aa430c0b95ed1cf89445df61a908aa86f273
Timur Çelikel <me@timurcelikel.com>
2018-06-29 18:12:54 +0200

    index: remove project list

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,23 +8,10 @@ import Layout from '../components/layout';
 class BlogIndex extends React.Component {
     render() {
         const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-        const posts = get(this, 'props.data.allMarkdownRemark.edges');
 
         return (
             <Layout location={this.props.location}>
                 <Helmet title={siteTitle} />
-                {posts.map(({ node }) => {
-                    const title = get(node, 'frontmatter.title') || node.fields.slug;
-                    return (
-                        <div key={node.fields.slug}>
-                            <h3>
-                                <Link to={node.fields.slug}>{title}</Link>
-                            </h3>
-                            <small>{node.frontmatter.year}</small>
-                            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-                        </div>
-                    );
-                })}
             </Layout>
         );
     }
@@ -37,21 +24,6 @@ export const pageQuery = graphql`
         site {
             siteMetadata {
                 title
-            }
-        }
-        allMarkdownRemark(sort: { fields: [frontmatter___order], order: DESC }) {
-            edges {
-                node {
-                    excerpt
-                    fields {
-                        slug
-                    }
-                    frontmatter {
-                        title
-                        year
-                        order
-                    }
-                }
             }
         }
     }
