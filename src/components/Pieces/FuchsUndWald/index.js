66e1df8b01b33d13764dd24227091c4ae10d182a
Timur Çelikel <me@timurcelikel.com>
2018-07-03 09:11:45 +0200

    Pieces: Fuchs und Wald performance improvement

--- src/components/Pieces/FuchsUndWald/index.js
+++ src/components/Pieces/FuchsUndWald/index.js
@@ -5,41 +5,37 @@ import classNames from 'classnames';
 const COUNT = 8000;
 const TIME_TO_GROW = 5;
 const INTERVAL = 10;
-const GAPS = 2;
+const GAPS = 5;
 
 export default class FuchsUndWald extends React.Component {
     constructor(props) {
         super(props);
 
-        this.forest = Array(COUNT)
-            .fill(0)
-            .map(tree => {
-                return Math.random();
+        this.forests = Array(INTERVAL).fill('');
+
+        for (let i = 0; i <= COUNT; i++) {
+            const forestIndex = Math.round(Math.random() * (INTERVAL + GAPS));
+
+            this.forests.forEach((forest, index, forests) => {
+                forests[index] = forest + (forestIndex === index ? '∧' : ' ');
             });
+        }
     }
     render() {
-        const forests = [];
-        const steps = 1 / INTERVAL;
-
-        for (let i = 0; i <= INTERVAL - GAPS; i++) {
-            const forestMin = steps * i;
-            const forestMax = steps * (i + 1);
-            const delay = steps * i * TIME_TO_GROW + 's';
-
-            forests.push(
-                <div key={i} style={{ animationDelay: delay }} className={s.forest}>
-                    {getForest(this.forest, forestMin, forestMax)}
-                </div>
-            );
-        }
-        return <div className={s.container}>{forests}</div>;
+        return (
+            <div className={s.container}>
+                {this.forests.map((forest, i) => {
+                    return (
+                        <div
+                            key={i}
+                            style={{ animationDelay: (TIME_TO_GROW / INTERVAL) * i + 's' }}
+                            className={s.forest}
+                        >
+                            {forest}
+                        </div>
+                    );
+                })}
+            </div>
+        );
     }
-}
-
-function getForest(forest, forestMin, forestMax) {
-    return forest
-        .map(tree => {
-            return tree > forestMin && tree < forestMax ? '∧' : ' ';
-        })
-        .join('');
 }
