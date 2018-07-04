b3f245ee46b7ace68c05d3955151f420cc58fd52
Timur Çelikel <me@timurcelikel.com>
2018-07-04 14:41:22 +0200

    import background in JS for now

--- src/components/Pieces/goldencity/index.js
+++ src/components/Pieces/goldencity/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './style.module.css';
+import boa from './boa.png';
 
 export default function() {
-    return <div className={s.boa} />;
+    return <div style={{ backgroundImage: `url(${boa})` }} className={s.boa} />;
 }
