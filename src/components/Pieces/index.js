37e177efec0d9469a3544f634158a3a75e419473
Timur Çelikel <me@timurcelikel.com>
2018-07-11 11:36:05 +0200

    add transformation des alternativen

--- src/components/Pieces/index.js
+++ src/components/Pieces/index.js
@@ -2,10 +2,12 @@ import goldencity from './goldencity';
 import FuchsUndWald from './FuchsUndWald';
 import FastForwardScience from './FastForwardScience';
 import VolligOhne from './VolligOhne';
+import TransformationDesAlternativen from './TransformationDesAlternativen';
 
 export default {
     '/goldencity/': goldencity,
     '/fastforwardscience/': FastForwardScience,
     '/fuchsundwald/': FuchsUndWald,
     '/about/': VolligOhne,
+    '/transformationdesalternativen/': TransformationDesAlternativen,
 };
