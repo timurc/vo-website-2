2893efefdd63da6acd2863ad17aa8190546bb5eb
Timur Çelikel <me@timurcelikel.com>
2018-09-13 10:21:41 +0200

    remove console.log

--- src/components/Img/index.js
+++ src/components/Img/index.js
@@ -3,8 +3,6 @@ import classNames from 'classnames';
 import s from './style.module.less';
 
 export default function Img({ srcSet, base64, className, aspectRatio }) {
-    console.log(aspectRatio);
-
     return (
         <div style={{ paddingBottom: `${100 / aspectRatio}%` }} className={classNames(className, s.container)}>
             <img className={s.image} sizes="calc(100vw - 20rem)" src={base64} srcSet={srcSet} />
