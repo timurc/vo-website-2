c0bdba7000bd6d73c17e841b96ea0a30ba0d633d
Timur Çelikel <me@timurcelikel.com>
2018-10-10 11:13:57 +0200

    news: add images

--- src/components/Img/index.js
+++ src/components/Img/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 import s from './style.module.less';
 
-export default function Img({ srcSet, base64, className, aspectRatio }) {
+export default function Img({ srcSet, base64, className, aspectRatio, sizes = 'calc(100vw - 20rem)' }) {
     return (
         <div style={{ paddingBottom: `${100 / aspectRatio}%` }} className={classNames(className, s.container)}>
-            <img className={s.image} sizes="calc(100vw - 20rem)" src={base64} srcSet={srcSet} />
+            <img className={s.image} sizes={sizes} src={base64} srcSet={srcSet} />
         </div>
     );
 }
--- src/components/News/index.js
+++ src/components/News/index.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 import get from 'lodash/get';
 import { StaticQuery, graphql } from 'gatsby';
 import Helmet from 'react-helmet';
+import Img from './../Img';
 
 import s from './style.module.less';
 
@@ -17,8 +18,6 @@ class News extends React.Component {
                         site {
                             siteMetadata {
                                 title
-                                description
-                                siteUrl
                             }
                         }
                         allMarkdownRemark(
@@ -35,6 +34,15 @@ class News extends React.Component {
                                         year
                                         date
                                         description
+                                        mainImage {
+                                            childImageSharp {
+                                                fluid {
+                                                    srcSet
+                                                    aspectRatio
+                                                    base64
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -55,12 +63,17 @@ class News extends React.Component {
 
 function Template({ data, location, children }) {
     const news = get(data, 'allMarkdownRemark.edges');
+
     return (
         <ul>
             {news.map(news => {
+                const img = get(news, 'node.frontmatter.mainImage.childImageSharp.fluid');
                 return (
                     <li key={news.node.fields.slug}>
-                        <Link to={news.node.fields.slug}>{news.node.frontmatter.title}</Link>
+                        <Link className={s.newsLink} to={news.node.fields.slug}>
+                            <Img sizes="(max-width: 450px) 100vw, 450px" {...img} />
+                            {news.node.frontmatter.title}
+                        </Link>
                     </li>
                 );
             })}
