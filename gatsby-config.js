6de4c3b7271fdfd4e672750d94c12530f61f46ca
Timur Çelikel <me@timurcelikel.com>
2018-10-10 23:10:21 +0200

    add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,61 @@ module.exports = {
                 //trackingId: `ADD YOUR TRACKING ID HERE`,
             },
         },
-        // `gatsby-plugin-feed`,
+
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign({}, edge.node.frontmatter, {
+                                    description: edge.node.excerpt,
+                                    url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                                    custom_elements: [{ 'content:encoded': edge.node.html }],
+                                });
+                            });
+                        },
+                        query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: { fields: { slug: { regex: "/^/neuigkeiten/" } } }
+              ) {
+                edges {
+                  node {
+                    excerpt
+                    html
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+                        output: '/rss.xml',
+                        title: 'Gatsby RSS Feed',
+                    },
+                ],
+            },
+        },
         `gatsby-plugin-offline`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-less`,
